7a950dea758be39cbdc8aaf26d1d387d122f5a20
westernat <westernat233@163.com>
2025-12-25 15:27:04 +0800

    改变量名

--- src/main/resources/assets/minecraft/shaders/include/tdp_utils.glsl
+++ src/main/resources/assets/minecraft/shaders/include/tdp_utils.glsl
@@ -1,12 +1,12 @@
 #version 150
 
 ivec2 max_light(int light) {
-    int bsl = light % 16;
+    int ebl = light % 16;
     int esl = (light / 16) % 16;
-    int bsl = (light / 256) % 16;
+    int mbl = (light / 256) % 16;
     int msl = light / 4096;
 
-    int sl = max(esl, msl);
-    int bl = max(bsl, bsl);
+    int sl = max(msl, esl);
+    int bl = max(mbl, ebl);
     return ivec2(sl, bl);
 }
